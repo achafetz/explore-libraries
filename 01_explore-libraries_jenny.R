859b6a3d546a399328617cc4f50a6a9701f7583f
Aaron <achafetz@usaid.gov>
2018-01-31 14:24:55 -0800

    moved libraries to top

--- 01_explore-libraries_jenny.R
+++ 01_explore-libraries_jenny.R
@@ -2,6 +2,11 @@
 #' output: github_document
 #' ---
 
+
+#libraries
+library(fs)
+library(tidyverse)
+
 ## how jenny might do this in a first exploration
 ## purposely leaving a few things to change later!
 
@@ -10,11 +15,10 @@
 
 ## let's confirm the second element is, in fact, the default library
 .Library
-library(fs)
+
 path_real(.Library)
 
 #' Installed packages
-library(tidyverse)
 ipt <- installed.packages() %>%
   as_tibble()
 
